fec756cb0893b0a4567c7cf6fba2f97ad7c06b44
yuuki <yuuki@yuukikonno.com>
2026-08-04 18:40:18 +0900

    Change post hash to rows hash in twilog.sh

--- twilog.sh
+++ twilog.sh
@@ -5,12 +5,12 @@ perl -MText::CSV_XS=csv -MHTML::Entities=decode_entities -E '
     ($year) = $row->[2] =~ /^(\d{4})/;
     $body = decode_entities($row->[3]);
     $body =~ s/\n/\x{2424}/g;
-    push @{ $post{$year} }, [$row->[2], $body, $row->[1]];
+    push @{ $rows{$year} }, [$row->[2], $body, $row->[1]];
   }
   for $year (2021 .. 2026) {
-    next unless @{ $post{$year} };
+    next unless @{ $rows{$year} };
     csv(
-      in => $post{$year},
+      in => $rows{$year},
       encoding => "utf-8",
       sep_char => "\t",
       eol => "\n",
