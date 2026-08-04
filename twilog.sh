4a60670d379f37975be9e5f77e41f2d0034c9cf6
yuuki <yuuki@yuukikonno.com>
2026-08-04 18:37:33 +0900

    Create twilog.sh to convert CSV to TSV
    This script processes CSV files and generates TSV files for the years 2021 to 2026.

---
+++ twilog.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+perl -MText::CSV_XS=csv -MHTML::Entities=decode_entities -E '
+  $csv = csv(in => shift, encoding => "utf-8");
+  for $row (reverse @$csv) {
+    ($year) = $row->[2] =~ /^(\d{4})/;
+    $body = decode_entities($row->[3]);
+    $body =~ s/\n/\x{2424}/g;
+    push @{ $post{$year} }, [$row->[2], $body, $row->[1]];
+  }
+  for $year (2021 .. 2026) {
+    next unless @{ $post{$year} };
+    csv(
+      in => $post{$year},
+      encoding => "utf-8",
+      sep_char => "\t",
+      eol => "\n",
+      quote_char => undef,
+      escape_char => undef,
+      out => "$year.tsv",
+    );
+  }
+' yuuki26-*.csv
